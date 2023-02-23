f7952a72f7b71ca315e99498c56e9a3cf1f8c50e
Tanya <tetiana.tatarova@gmail.com>
2023-02-23 16:36:57 +0200

    homework API

---
+++ Homework_Lesson_22_API/main.js
@@ -0,0 +1,29 @@
+const apiKey = 'e0dce53e909d40b5b8ff77421dc31a93';
+const url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=${apiKey}`;
+let data;
+const news = document.querySelector('.news');
+
+fetch(url)
+	.then((response) => response.json())
+  .then((json) => {
+    data = json.articles;    
+  })
+  .then(() => {onDataRender()});
+
+function onDataRender() {
+  data.forEach(function (el, index) {    
+		let html = `<div class="news__item">
+				<img class="news__item-img" src = ${el.urlToImage}>
+        <div class="news__item-info">
+				<h2 class="news__item-title">${el.title}</h2>
+        <h3 class="news__item-subtitle">Author: <span>${el.author}</span></h3>
+				<div class="news__item-description">${el.description}</div>
+        <a class="news__item-link" href="${el.url}" target="_blank">Read more...</a>
+        </div>				
+				</div>`;
+		if (index <= 30) {
+			news.insertAdjacentHTML('beforeend', html);
+		}
+	});
+};
+  
